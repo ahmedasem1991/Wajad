9309e9194520fad1de35909355db0fc8ca770247
Abdelhammied Elsayed <abdelhammied@gmail.com>
2019-07-31 14:58:14 +0200

    add excel download package

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -5,9 +5,10 @@ namespace App\Nova;
 use Laravel\Nova\Fields\ID;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\Gravatar;
 use Laravel\Nova\Fields\Password;
-use Laravel\Nova\Fields\HasMany;
+use Maatwebsite\LaravelNovaExcel\Actions\DownloadExcel;
 
 class User extends Resource
 {
@@ -107,6 +108,8 @@ class User extends Resource
      */
     public function actions(Request $request)
     {
-        return [];
+        return [
+            new DownloadExcel,
+        ];
     }
 }
