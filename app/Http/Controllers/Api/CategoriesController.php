0556a04e106b71a8944c5c3fbe7c5b49692910b5
Abdelhammied Elsayed <abdelhammied@gmail.com>
2019-08-05 18:52:28 +0200

    categories starts

---
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $categories = QueryBuilder::for(Category::class)
+            ->allowedIncludes('items')
+            ->paginate($request->get('per_page', 15));
+
+        $this->addResponse($categories)->addStatusCode(200);
+
+        return $this->response();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,11 @@ use Illuminate\Http\Request;
 Route::post('/login', 'Auth\AuthController@login');
 Route::post('/register', 'Auth\AuthController@register');
 
-# Sliders Starts
-
+Route::group(['middleware' => ['auth:api']], function(){
+    # Categories 
+    Route::get('/categories', 'CategoriesController@index');
+    
+});
 Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/user', function (Request $request) {
         return auth('api')->user();
