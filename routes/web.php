4ce005f6a71d596f5ce9abe7cd12edf1f423f8c2
abdelhammied <abdelhammied@gmail.com>
2019-10-03 11:07:20 +0200

    random commit

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,4 @@
 <?php
-use App\Settings;
-use Illuminate\Support\Str;
-use App\Region;
-use App\Package;
-use App\Item;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,31 +15,7 @@ use App\Item;
 // 	return Settings::find('about-us');
 // });
 
-Route::resource('user', 'UserController');
-Route::resource('item', 'ItemController');
-Route::resource('category', 'CategoryController');
-Route::resource('itemimages', 'ItemImagesController');
-Route::resource('questions', 'QuestionsController');
-Route::resource('answers', 'AnswersController');
-Route::resource('itemrequests', 'ItemRequestsController');
-Route::resource('products', 'ProductsController');
-Route::resource('cards', 'CardsController');
 Route::get('{slug}/{param?}', '\Remipou\NovaPageManager\PageController@page')
 	->where('slug', '^((?!' . trim(config('nova.path'), '/') . '|nova-).)*$')
 	->name('page-manager');
 Auth::routes();
-
-Route::get('/test', function(){
-	// return htmlspecialchars(Item::where('id', 100)->first());
-	// $nexmo = app('Nexmo\Client');
-	// $nexmo->message()->send([
-	// 	'to'   => '201095781611',
-	// 	'from' => 'nexmo',
-	// 	'text' => 'Using the facade to send a message.'
-	// ]);
-	//getimagesize('');
-
-	return Package::packagesPeriod();
-});
-
-Route::get('/home', 'HomeController@index')->name('home');
