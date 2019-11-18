96d95297a9ee49accb85ac431a0856b72264d76e
vagrant <vagrant@homestead>
2019-11-18 13:50:40 +0000

    resources

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,22 +2,14 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Post;
-use Response;
-use App\Brand;
-use App\Color;
-use App\Model;
 use App\Category;
-use App\SubCategory;
 use Illuminate\Http\Request;
-use Spatie\QueryBuilder\Filter;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoryResource;
-use Spatie\QueryBuilder\QueryBuilder;
 
 class CategoryController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         return CategoryResource::collection(Category::all());
     }
