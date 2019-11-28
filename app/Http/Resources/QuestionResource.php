b06f1f28d3d6164d61ce0a0ae82cacbcac7cb0d5
vagrant <vagrant@homestead>
2019-11-28 14:49:17 +0000

    cretae post

---
+++ app/Http/Resources/QuestionResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QuestionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'founder_id' => new UserResource($this->user),
+            'question' => $this->question,
+        ];
+    }
+}
