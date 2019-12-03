fe80ff4195161a04e5992028245bb30cab0e2225
Abdelhammied <abdelhammied@gmail.com>
2019-12-03 16:32:16 +0000

    fixing phone in user service

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -91,8 +91,7 @@ class UserService
         }
 
         if ($code_valid_for == 'email') {
-            Mail::to($user)
-                ->send(new EmailVerificationCode($activation_code));
+            // Mail::to($user)->send(new EmailVerificationCode($activation_code));
 
             return true;
         }
