d35f6d646b692b7b8fa105888b65c3dc6241c521
Abdelhammied <abdelhammied@gmail.com>
2019-12-04 16:15:55 +0000

    adding seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,5 +11,19 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(PageSeeder::class);
+
+        $users = factory(App\Item::class, 15)
+            ->create()
+            ->each(function ($user) {
+                $user->model()->create(
+                    factory(App\Model::class, 5)->create()->id
+                );
+                $user->color()->create(
+                    factory(App\Color::class, 5)->create()->id
+                );
+                $user->owner()->create(
+                    factory(App\User::class, 5)->create()->id
+                );
+            });
     }
 }
