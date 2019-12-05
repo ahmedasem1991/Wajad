46da0fe66fa376cc2cc7db22619aa50b47671320
Abdelhammied <abdelhammied@gmail.com>
2019-12-05 13:54:51 +0000

    change banner api

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -212,6 +212,7 @@ class AuthController extends Controller
         return response()->json([
             'token_type' => 'Bearer',
             'access_token' => $token,
+            'refresh_token' => auth('api')->user()->remember_token,
             'expires_in' => config('jwt.ttl') * 60,
             'user' => new UserResource(auth('api')->user())
         ]);
