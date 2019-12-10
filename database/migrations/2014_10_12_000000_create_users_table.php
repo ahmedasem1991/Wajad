f4c4e83ee7db6b22c6986a96cc1e2d22afd785cb
Rehab <r.ahmed@smartappco.com>
2019-12-10 14:39:11 +0000

    item qrcode

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -37,7 +37,7 @@ class CreateUsersTable extends Migration
             $table->boolean('is_mobile_number_verified')->default(false);
 
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('image', 500)->nullable();
+            $table->string('image', 500)->default(" ");
             $table->rememberToken();
             $table->timestamps();
         });
