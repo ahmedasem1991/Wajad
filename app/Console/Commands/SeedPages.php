6e3c6b4d55c78c6e99e25f3f7a5fd47558bfd44a
Rehab <r.ahmed@smartappco.com>
2019-12-11 11:22:21 +0000

    codes

--- app/Console/Commands/SeedPages.php
+++ app/Console/Commands/SeedPages.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Page;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -44,17 +45,17 @@ class SeedPages extends Command
         });
         $Pages = [
             [
-                'template' => 'about_us',
-                'name' => 'about_us',
-                'title' => 'about_us',
-                'slug' => 'about_us',
-                'meta_title' => 'about_us',
-                'meta_description' => 'about_us',
-                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio autem ducimus nemo rem. Placeat aliquam delectus itaque illo nobis facere explicabo corporis qui praesentium nemo voluptates quod harum vel, culpa nesciunt sint tempora maxime non. Quidem nihil iure tenetur minus quia sint totam dolores magnam illo! Unde repellendus vitae consequuntur.',
-                'created_at' => null,
-                'updated_at' => null
+                // 'template' => 'about_us',
+                'key' => 'about_us',
+                'title_en' => 'about_us',
+                'title_ar' => 'about_us',
+                'body_en' => 'about_us',
+                'body_ar' => 'about_us',
+                // 'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio autem ducimus nemo rem. Placeat aliquam delectus itaque illo nobis facere explicabo corporis qui praesentium nemo voluptates quod harum vel, culpa nesciunt sint tempora maxime non. Quidem nihil iure tenetur minus quia sint totam dolores magnam illo! Unde repellendus vitae consequuntur.',
+                'created_at' => Carbon::now()->toDateTimeString(),
+                'updated_at' => Carbon::now()->toDateTimeString(),
             ],
-           
+
         ];
 
         foreach ($Pages as $Page) {
