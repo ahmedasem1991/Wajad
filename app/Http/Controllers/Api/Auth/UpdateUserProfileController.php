cbd5144303bcfbd93ba15eaf56c04b3a07289eb5
Rehab <r.ahmed@smartappco.com>
2019-12-15 10:24:28 +0000

    image

--- app/Http/Controllers/Api/Auth/UpdateUserProfileController.php
+++ app/Http/Controllers/Api/Auth/UpdateUserProfileController.php
@@ -59,7 +59,7 @@ class UpdateUserProfileController extends Controller
             'default_distance_unit' => $request->default_distance_unit,
         ]);
 
-        if ($request->has('image') && $request->image !== '') {
+        if ($request->has('image') && $request->image !== '' && !is_null($request->image)) {
             Storage::disk('public')->delete($user->image);
 
             $user->update([
