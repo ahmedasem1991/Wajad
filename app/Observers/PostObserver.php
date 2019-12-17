be72727af8fe04f0502dcedf3a9d68d7e33c6627
Rehab <r.ahmed@smartappco.com>
2019-12-17 09:19:48 +0000

    post observer

--- app/Observers/PostObserver.php
+++ app/Observers/PostObserver.php
@@ -21,15 +21,13 @@ class PostObserver
      */
     public function saving(Post $Post)
     {
-        //  if(Auth()->User()->isCorporateUser() ||Auth()->User()->isCorporateAdmin() )
-        //  {
+        if (Auth()->User()->isCorporateUser() || Auth()->User()->isCorporateAdmin()) {
 
-        // $Post->appearance_status = 1;
-        // $Post->open_status = 1;
-        // $Post->approval_status = 1;
-        // $Post->corporate_id = Auth()->User()->corporate_id;
-        //  }
-
+            $Post->appearance_status = 1;
+            $Post->open_status = 1;
+            $Post->approval_status = 1;
+            $Post->corporate_id = Auth()->User()->corporate_id;
+        }
     }
     public function saved(Post $Post)
     { }
