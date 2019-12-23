acc603e1eba6c2372566d48353da1fc999fc89ef
Rehab <r.ahmed@smartappco.com>
2019-12-23 09:52:00 +0000

    seeds

---
+++ app/Console/Commands/SeedUsers.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use App\Corporate;
+use Illuminate\Console\Command;
+
+class SeedUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'seed:users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        $this->info('Create Nova Admin');
+        $username = $this->ask('Username', 'Admin');
+        $email = $this->ask('Email Address', 'admin@nova.com');
+        $password = $this->ask('Password', 123456789);
+        User::create([
+            'name' => $username,
+            'email' => $email,
+            'password' => bcrypt($password),
+            'type' => 3, // Admin
+            'mobile_number' => '01111086890',
+            'mobile_country_id' => 1
+        ]);
+        $this->info('Nova Admin Created Successfully');
+
+        $this->info('Create Nova User');
+        $username = $this->ask('Username', 'User');
+        $email = $this->ask('Email Address', 'user@nova.com');
+        $password = $this->ask('Password', 123456789);
+        User::create([
+            'name' => $username,
+            'email' => $email,
+            'password' => bcrypt($password),
+            'type' => 1, // User
+            'mobile_number' => '01142416124',
+            'corporate_id' => 1,
+            'mobile_country_id' => 1,
+            'posts_limitation' => 50
+        ]);
+        $this->info('Nova User Created Successfully');
+
+
+        $this->info('Create Nova Corporate Admin');
+        $username = $this->ask('Username', 'Corporate');
+        $email = $this->ask('Email Address', 'corporate@nova.com');
+        $password = $this->ask('Password', 123456789);
+        User::create([
+            'name' => $username,
+            'email' => $email,
+            'password' => bcrypt($password),
+            'type' => 2, // Corporate
+            'mobile_number' => '+201095781611',
+            'corporate_id' => 1,
+            'mobile_country_id' => 1
+        ]);
+        Corporate::create([
+            'name_en' => 'WAJAD Corporate',
+            'name_ar' => 'مؤسسة وجد',
+            'details_en' => 'WAJAD Corporate For Haj & Omra',
+            'details_ar' => 'مؤسسة وجد للحج والعمرة', // User
+            'address_en' => 'Jadda - KSA',
+            'address_ar' => 'جده - المملكة العربية السعودية',
+            'latitude' => '21.4498898',
+            'longitude' => '39.4913423',
+            'status' => 1,
+
+        ]);
+        $this->info('Nova Corporate Admin Created Successfully');
+    }
+}
