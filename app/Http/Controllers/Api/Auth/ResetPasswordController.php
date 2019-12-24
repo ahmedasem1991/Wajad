93aa4c4e314a553e9ff9b0f4616d54765c4ae93a
Rehab <r.ahmed@smartappco.com>
2019-12-24 12:41:03 +0000

    dd

--- app/Http/Controllers/Api/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Api/Auth/ResetPasswordController.php
@@ -54,9 +54,9 @@ class ResetPasswordController extends Controller
         $user = User::where($user_identifier, '=', request('user'))
             ->where('type', '=', User::Types['user'])
             ->first();
-            
+
         if (!$user) {
-            throw new ApiException(trans('messages.notfound', ['model' => trans('messages.attributes.user')]), 404);
+            throw new ApiException(trans('messages.not_found', ['model' => trans('messages.attributes.user')]), 400);
         }
 
         $new_password = env('STATIC_NEW_PASSWORD', \Str::upper(\Str::random(6)));
