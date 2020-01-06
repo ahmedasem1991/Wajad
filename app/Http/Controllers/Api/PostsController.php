0ee6933d1d28a9594cd1bdcae7cf68640b750bc8
Rehab <r.ahmed@smartappco.com>
2020-01-06 11:49:08 +0000

    fixing quesions

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -83,7 +83,7 @@ class PostsController extends Controller
         if ($type == "found") {
             $validate_questions = Validator::make($request->all(), [
                 'questions' => ['required',  'array', 'between:1,3'],
-                'questions.*' => ['required', 'min:9', 'max:500'],
+                'questions.*' => ['nullable', 'min:9', 'max:500'],
             ]);
             if ($validate_questions->fails()) {
                 throw new ApiException($validate_questions->errors()->first(), 400);
