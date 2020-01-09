f49c2c41d58d109a87deab2c8230b4865a35a855
Rehab <r.ahmed@smartappco.com>
2020-01-09 11:27:05 +0000

    pdf recipt

--- resources/views/Pdf/receipt.blade.php
+++ resources/views/Pdf/receipt.blade.php
@@ -1,4 +1,20 @@
-@extends('layouts.layout')
-@section('content')
-dddd{{ $post }}
-@endsection
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8"> 
+    <title>Wajad</title>
+</head>
+<body>
+  <div class="container">
+    <header>
+      @include('partials.header')
+    </header>
+    <div id="main" class="row" style="float:right; text-align:right;">
+    {{$post->title}}
+    </div>
+    <footer>
+        @include('partials.footer')  
+    </footer>
+  </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -102,7 +102,7 @@ Route::get('/home', function () {
     return  redirect(Nova::path());
 });
 
-Route::view('qrcode', 'Pdf.qrcode');
+Route::view('qrcode', 'Pdf.qrcode'); 
 Auth::routes();
 //Test Notification
 Route::get('/sendfcm', 'NotificationController@sendFCM');
