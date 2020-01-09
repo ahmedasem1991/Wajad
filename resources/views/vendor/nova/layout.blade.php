34fa5b9f41fa4a59bfe2b8c97a22e9a203325e89
Abdelhammied Elsayed <abdelhammied@gmail.com>
2020-01-09 10:50:13 +0000

    well done

--- resources/views/vendor/nova/layout.blade.php
+++ resources/views/vendor/nova/layout.blade.php
@@ -1,1 +1,5 @@
-@extends(Auth::user()->adminFirstLogin() || Auth::user()->corporateAdminFirstLogin() ? 'auth.update_password' : 'nova::masterLayout' )
+@if (App::environment('local'))
+    @extends('nova::masterLayout')
+@else
+    @extends(Auth::user()->adminFirstLogin() || Auth::user()->corporateAdminFirstLogin() ? 'auth.update_password' : 'nova::masterLayout' )
+@endif
