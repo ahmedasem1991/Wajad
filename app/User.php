4077ef20b222729b58039dd63ae915f6f8708f68
IbrahemSaber <i.saber@smartappco.com>
2020-01-14 16:50:24 +0200

    1

--- app/User.php
+++ app/User.php
@@ -227,7 +227,7 @@ class User extends Authenticatable implements JWTSubject, MustVerifyEmail
     public function exceededPostLimitation()
     {
         logger($this->posts_number);
-        logger(defaultGroup()->limitation_of_posts);
+        logger(defaultGroup());
             return $this->posts_number > defaultGroup()->limitation_of_posts;
     }
 
