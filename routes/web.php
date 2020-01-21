5c271600d1d2e60fe54ae6348dd0653555448848
IbrahemSaber <i.saber@smartappco.com>
2020-01-21 14:34:29 +0200

    add redirect

--- routes/web.php
+++ routes/web.php
@@ -138,8 +138,8 @@ Route::domain(config('nova.domain', null))
         Route::post('/updatePassword', 'UpdatePasswordController@updatePassword')->name('update_password');
     });
 route::get('/', function () {
-    return 'Welcome To Wajad';
-   // return redirect(Nova::path());
+   // return 'Welcome To Wajad';
+    return redirect(Nova::path());
 });
 
 
