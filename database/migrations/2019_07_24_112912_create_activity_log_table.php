99d423e4cf7009865fccafcb9ec154dd2f965933
Ibrahem Saber <i.saber@smartappco.com>
2020-01-24 03:15:11 +0000

    Update 2019_07_24_112912_create_activity_log_table.php

--- database/migrations/2019_07_24_112912_create_activity_log_table.php
+++ database/migrations/2019_07_24_112912_create_activity_log_table.php
@@ -20,7 +20,7 @@ class CreateActivityLogTable extends Migration
             $table->string('subject_type')->nullable();
             $table->unsignedBigInteger('causer_id')->nullable();
             $table->string('causer_type')->nullable();
-           // $table->json('properties')->nullable();
+            $table->json('properties')->nullable();
          //  $table->string('properties')->nullable();
            $table->softDeletes();
 $table->timestamps();
