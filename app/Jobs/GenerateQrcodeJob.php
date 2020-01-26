ec5c3d370805a3c290bf1f7b00cac0ff2385ba9d
IbrahemSaber <i.saber@smartappco.com>
2020-01-26 11:38:58 +0200

    remove logo from qrcode

--- app/Jobs/GenerateQrcodeJob.php
+++ app/Jobs/GenerateQrcodeJob.php
@@ -51,7 +51,7 @@ class GenerateQrcodeJob implements ShouldQueue
            $Url=$this->id.time().str_random(20);
             \QrCode::backgroundColor(255, 255, 0)->color(255, 0, 127)
             ->format('png')
-            ->merge(public_path('/images/'.env('QRCODE_LOGO','logo.png')), 0.1, true)
+           // ->merge(public_path('/images/'.env('QRCODE_LOGO','logo.png')), 0.1, true)
             ->size(2000)
             ->generate(env('API_URL').'/scan-qr-code/'.$Url,
             public_path('images/qrcodes/'.$ImageName));
