636e9437b3e9a31e7483a795859064e9cff4b6ef
Abdelhammied Elsayed <abdelhammied@gmail.com>
2020-02-09 15:10:59 +0200

    fix error

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -471,7 +471,7 @@ class PostsController extends Controller
                 $post_images = [];
                 foreach ($request->images as $image) {
 
-                    return response()->json([(string) Str::startsWith($image, 'data:image')]);
+                    return response()->json([Str::startsWith($image, 'data:image')]);
 
                     if (Str::startsWith($image, 'data:image')) {
                         $image_name = Str::random(15) . '.' . 'png';
