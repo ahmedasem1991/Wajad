ca75547aab1e44970b4e1d15ea74f84bfcb37f2c
IbrahemSaber <i.saber@smartappco.com>
2020-03-09 13:35:05 +0200

    add frist to settings

--- app/Http/Resources/ContactusResource.php
+++ app/Http/Resources/ContactusResource.php
@@ -18,12 +18,12 @@ class ContactusResource extends JsonResource
         return [
             // 'key' => $this['key'],
             // 'value' =>$this['value'],
-           'Phone-Number-1' => $this->where(['key'=> 'Phone-Number-1'])->pluck('value') ?? '',
-           'Phone-Number-2' => $this->where(['key'=> 'Phone-Number-2'])->pluck('value') ?? '',
-           'Address-1' => $this->where(['key'=> 'Address-1'])->pluck('value') ?? '',
-           'Address-2' => $this->where(['key'=> 'Address-2'])->pluck('value') ?? '',
-           'Email-1' => $this->where(['key'=> 'Email-1'])->pluck('value') ?? '',
-           'Email-2' => $this->where(['key'=> 'Email-2'])->pluck('value') ?? '',
+           'Phone-Number-1' => $this->where(['key'=> 'Phone-Number-1'])->pluck('value')->first() ?? '',
+           'Phone-Number-2' => $this->where(['key'=> 'Phone-Number-2'])->pluck('value')->first() ?? '',
+           'Address-1' => $this->where(['key'=> 'Address-1'])->pluck('value')->first() ?? '',
+           'Address-2' => $this->where(['key'=> 'Address-2'])->pluck('value')->first() ?? '',
+           'Email-1' => $this->where(['key'=> 'Email-1'])->pluck('value')->first() ?? '',
+           'Email-2' => $this->where(['key'=> 'Email-2'])->pluck('value')->first() ?? '',
         ];
     }
 }
