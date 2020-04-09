04a6da7e7c467ece634d565279226c32b358034b
IbrahemSaber <i.saber@smartappco.com>
2020-04-09 21:10:32 +0200

    solve not item

--- resources/views/webview/index.blade.php
+++ resources/views/webview/index.blade.php
@@ -18,11 +18,21 @@
                 <div class="card border-0 mb-4">
                     <div class="card-body">
                             <span class="badge badge-danger rounded-pill px-3 py-1 font-weight-bold">Date
-                                <span class="font-weight-light">{{$qr_code->item->created_at}}</span>
+                                <span class="font-weight-light">
+                                @if($qr_code->item)
+                                {{$qr_code->item->created_at}}
+                                @endif
+                                </span>
                             </span>
                         <h3 class="my-3 text-uppercase">
-                            <span class="font-weight-bold">{{$qr_code->item->title}}</span>
+                            <span class="font-weight-bold">
+                            @if($qr_code->item)
+                                {{$qr_code->item->title}}
+                                @endif
+                            </span>
+                            @if($qr_code->item)
                             {{$qr_code->item->color->name_en}}
+                            @endif
                         </h3>
                         <div class="row">
                             <div class="col-12 col-md-6">
@@ -53,7 +63,9 @@
 
 
                         <p class="my-4">
+                        @if($qr_code->item)
                             {{$qr_code->item->details}}
+                        @endif
                         </p>
                         <h5 class="my-3 mb-4">
                             <span class="font-weight-bold">Download Now</span>
@@ -78,9 +90,11 @@
                 </div>
             </div>
             <div class="col-lg-6">
+            @if($qr_code->item)
                 @foreach($qr_code->item->images as $image)
                     <img src="{{$image}}" alt="wrapkit" class="img-fluid rounded my-3" id="product-img" />
                 @endforeach
+                @endif
             </div>
         </div>
     </div>
