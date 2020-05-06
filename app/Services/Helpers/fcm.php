ed6e4a10a8aafd026a91ea142a6124922bcd3d8f
IbrahemSaber <i.saber@smartappco.com>
2020-05-06 16:20:11 +0200

    test post create fcm

--- app/Services/Helpers/fcm.php
+++ app/Services/Helpers/fcm.php
@@ -320,7 +320,7 @@ function sendCreatePostFCM($post,$badge,$type)
         'type' => 'post_'.$type,
         'deeplink' => 'post_'.$type,
         'image' =>null ,
-        'post' => null,
+        'post' => new PostResource($post),
         'item' => null,
         'url' => null ,
         'id' => $post->id,
--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,10 @@ use Illuminate\Http\Request;
 use App\Mail\EmailVerificationCode;
 use Illuminate\Support\Facades\App;
 use App\Exceptions\Api\ApiException;
+use App\Http\Resources\PostResource;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Route;
 //use Stichoza\GoogleTranslate\GoogleTranslate;
+use Illuminate\Support\Facades\Route;
 use LaravelFCM\Message\OptionsBuilder;
 use App\Notifications\TestNotification;
 use Illuminate\Support\Facades\Storage;
@@ -333,7 +334,8 @@ dd ($downstreamResponse);
 
 
 Route::get('/test400', function () {
-    $post=Post::find(11);
+    $post=Post::find(70);
+   return  new PostResource($post);
     
     if ($post->isFound()) 
       return  $type='post_found';
