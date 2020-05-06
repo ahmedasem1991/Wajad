634692adc481a19e4263210bb9e5eb329efec6fb
IbrahemSaber <i.saber@smartappco.com>
2020-05-06 16:41:23 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -336,8 +336,8 @@ dd ($downstreamResponse);
 
 Route::get('/test400', function () {
     $post=Post::find(70);
-    $item = Item::find(1);
-   return  new ItemResource($item);
+//     $item = Item::find(1);
+//    return  new ItemResource($item);
    //return  new PostResource($post);
     
     // if ($post->isFound()) 
