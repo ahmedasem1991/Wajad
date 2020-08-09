8a700430a58e51cfdc6203052cdef20302df4e0d
IbrahemSaber <i.saber@smartappco.com>
2020-08-09 13:34:39 +0200

    update postrequest

--- app/Observers/PostRequestObserver.php
+++ app/Observers/PostRequestObserver.php
@@ -23,14 +23,14 @@ class PostRequestObserver
 
 
             $PostRequests=PostRequest::where('post_id',$postRequest->post_id)->where('post_id','!=',$postRequest->post_id)->get();
-            if($postRequest->is_request_valid==1){
+       
                 foreach($PostRequests as $PostRequest)
                 {
                     $PostRequests->is_request_valid=0;
                     $postRequest->rejected_at=now()->toDatetimeString();
-                    $PostRequests->save();
+                   // $PostRequests->save();
                 }
-            }
+           
              
             }
            
@@ -62,7 +62,7 @@ class PostRequestObserver
             {
                 $PostRequests->is_request_valid=0;
                 $postRequest->rejected_at=now()->toDatetimeString();
-                $PostRequests->save();
+                //$PostRequests->save();
             }
         }
     }
