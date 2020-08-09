9cb41638a084cb3bc12922803c93f6c24f2206d6
IbrahemSaber <i.saber@smartappco.com>
2020-08-09 13:20:13 +0200

    update postrequest

--- app/Observers/PostRequestObserver.php
+++ app/Observers/PostRequestObserver.php
@@ -37,6 +37,25 @@ class PostRequestObserver
         //
     }
 
+        /**
+     * Handle the post request "updated" event.
+     *
+     * @param  \App\PostRequest  $postRequest
+     * @return void
+     */
+    public function updating(PostRequest $postRequest)
+    {
+        $PostRequests=PostRequest::where('post_id',$postRequest->post_id)->where('post_id','!=',$postRequest->post_id)->get();
+        if($postRequest->is_request_valid==1){
+            foreach($PostRequests as $PostRequest)
+            {
+                $PostRequests->is_request_valid=0;
+                $postRequest->rejected_at=now()->toDatetimeString();
+                $PostRequests->save();
+            }
+        }
+    }
+
     /**
      * Handle the post request "updated" event.
      *
