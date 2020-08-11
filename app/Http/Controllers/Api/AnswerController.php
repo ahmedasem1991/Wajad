d491ce8a5583aab489a63b6180de89386727c680
IbrahemSaber <i.saber@smartappco.com>
2020-08-11 15:42:13 +0200

    update test

--- app/Http/Controllers/Api/AnswerController.php
+++ app/Http/Controllers/Api/AnswerController.php
@@ -68,7 +68,7 @@ class AnswerController extends Controller
            //send FCM
            $badge =getBadge($post->founder);
            $data=sendPostRequestFCM($post->founder,auth('api')->user(),$post,$badge,$post_request->id);
-           $post->founder->notify(new SendFCMNotification($post->founder,$data));
+         //  $post->founder->notify(new SendFCMNotification($post->founder,$data));
  
         $this->addResponse(trans('messages.created', ['model' => trans('messages.attributes.post_request')]))->addStatusCode(201);
         return $this->response();
