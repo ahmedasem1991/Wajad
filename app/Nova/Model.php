0d7d0b957356eace83e3d8bb58c29a867643d5e4
IbrahemSaber <i.saber@smartappco.com>
2020-08-11 13:40:30 +0200

    update model

--- app/Nova/Model.php
+++ app/Nova/Model.php
@@ -89,7 +89,7 @@ class Model extends Resource
             NovaBelongsToDepend::make('Brand','brand',\App\Nova\Brand::class)
                 ->placeholder('Select Brand')
                 ->optionsResolve(function ($subcategory) {
-                    return $subcategory->brands;
+                    return $subcategory->brands->get();
                 })
                 ->rules('required')
                 ->dependsOn('Subcategory'),
