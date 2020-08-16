353cc309be4f8a5798fdbd240a9aa5403dd71a47
IbrahemSaber <i.saber@smartappco.com>
2020-08-16 16:37:23 +0200

    add country_code

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends JsonResource
             "name" =>  $this->name,
             "email" =>  $this->email,
             "status" => $this->status,
-            "mobile_number" => $this->mobile_number,
+            "mobile_number" => $this->country->country_code .$this->mobile_number,
             "receive_emails" => (bool) $this->receive_emails,
             "receive_push_notifications" => (bool) $this->receive_push_notifications,
             "is_email_verified" =>  (bool) $this->email_verified_at,
