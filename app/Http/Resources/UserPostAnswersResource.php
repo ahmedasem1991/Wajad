72836aed996d2690b98a2805905f73eb5f729714
IbrahemSaber <i.saber@smartappco.com>
2020-08-24 15:22:25 +0200

    update

--- app/Http/Resources/UserPostAnswersResource.php
+++ app/Http/Resources/UserPostAnswersResource.php
@@ -10,15 +10,14 @@ class UserPostAnswersResource extends JsonResource
     {
         $user = new UserResource($this->postRequestUser);
 
+        $answers=$this->post->questions()->answers()->where('user_id', $user->id)->get()['answers'];
         $questions = collect([
             'questions' => //QuestionResource::collection(
-                $this->post->questions()->with([
-                        'answers' => function ($query) use ($user) {
-                            return $query->where('user_id', $user->id)->select('answers')->get();
-                        }
-                    ])->select('id','question')->get()
+                $this->post->questions()->select('id','question')->get()
            // )
         ]);
-        return $questions->merge($user);
+        $questions->merge($answers);
+        $questions->merge($user);
+        return $questions;
     }
 }
