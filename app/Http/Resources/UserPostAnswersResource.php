4f75b28b4dcea8002d224f281875a07ed5bf64ac
IbrahemSaber <i.saber@smartappco.com>
2020-08-24 14:14:08 +0200

    remove UserPostAnswersResource

--- app/Http/Resources/UserPostAnswersResource.php
+++ app/Http/Resources/UserPostAnswersResource.php
@@ -11,13 +11,13 @@ class UserPostAnswersResource extends JsonResource
         $user = new UserResource($this->postRequestUser);
 
         $questions = collect([
-            'questions' => QuestionResource::collection(
+            'questions' => //QuestionResource::collection(
                 $this->post->questions()->with([
                         'answers' => function ($query) use ($user) {
                             return $query->where('user_id', $user->id);
                         }
                     ])->get()
-            )
+           // )
         ]);
         return $questions->merge($user);
     }
