b1b807b0264a422ba3e889d3efb336101310afb4
IbrahemSaber <i.saber@smartappco.com>
2020-08-30 12:27:24 +0200

    test

--- app/Http/Resources/PostRequestsResource.php
+++ app/Http/Resources/PostRequestsResource.php
@@ -8,14 +8,14 @@ class PostRequestsResource extends JsonResource
 {
     public function toArray($request)
     {
-       $this_request=$this;
-       
-       $array['questions'] =$this->post->questions()->with([
-        'answers' => function ($query) use ($this_request) {
-             $query->where('user_id', $this->user_id);
-        }
-])->select('id','question')->get();
-return ($array['questions']);
+        $this_request = $this;
+
+        $array['questions'] = $this->post->questions()->with([
+            'answers' => function ($query) use ($this_request) {
+                $query->where('user_id', $this->user_id)->select('id', 'answers');
+            }
+        ])->select('id', 'question')->get();
+        dd($array['questions']);
         return new UserPostAnswersResource($this);
 
         return [
