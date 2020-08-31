5bc6ef2a1164490f957391150fabf81410149549
IbrahemSaber <i.saber@smartappco.com>
2020-08-31 14:51:58 +0200

    update mobile number in corporate

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,12 +14,19 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
+        if(session()->get('corporate_publisher')=='true')
+        {
+            $mob=$this->mobile_number;
+        }
+        else{
+            $mob=$this->country ? $this->country->country_code .$this->mobile_number: '' .$this->mobile_number;
+        }
         return [
             'id' => $this->id,
             "name" =>  $this->name,
             "email" =>  $this->email,
             "status" => $this->status,
-            "mobile_number" => $this->country ? $this->country->country_code .$this->mobile_number: '' .$this->mobile_number,
+            "mobile_number" => $mob,
             "receive_emails" => (bool) $this->receive_emails,
             "receive_push_notifications" => (bool) $this->receive_push_notifications,
             "is_email_verified" =>  (bool) $this->email_verified_at,
