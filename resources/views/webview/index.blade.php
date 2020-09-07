14460ba85750bd9ca10d22ac34464b844c29fd8c
IbrahemSaber <i.saber@smartappco.com>
2020-09-07 10:07:48 +0200

    test

--- resources/views/webview/index.blade.php
+++ resources/views/webview/index.blade.php
@@ -27,7 +27,7 @@
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  alert('mobile');
  //window.href('android-app://com.smartappco.wajad,"_self"');
- window.location="intent://wajad?id=" + token + "#Intent;scheme=myappscheme;package=myapppackage;S.browser_fallback_url=https%3A%2F%2Fplay.google.com%2;end";
+ window.href="intent://wajad?id=" + token + "#Intent;scheme=myappscheme;package=myapppackage;S.browser_fallback_url=https%3A%2F%2Fplay.google.com%2;end";
 }
 else{
     alert('desktop');
