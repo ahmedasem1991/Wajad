202157b4e8da6dcee48677e91102baaf03676bf5
IbrahemSaber <i.saber@smartappco.com>
2020-09-09 12:34:47 +0200

    validation of calimers

--- app/Http/Resources/PostRequestsResource.php
+++ app/Http/Resources/PostRequestsResource.php
@@ -8,22 +8,17 @@ class PostRequestsResource extends JsonResource
 {
     public function toArray($request)
     {
-        // $array=[];
-        // $this_request = $this;
-        // $user = new UserResource($this->user_id);
-        // $array['questions'] = $this->post->questions()->with([
-        //     'answers' => function ($query) use ($this_request) {
-        //         $query->where('user_id', $this->user_id)->where('user_id', $this->user_id);
-        //     }
-        // ])->select('id', 'question')->get();
-        // return  $array;
-        return new UserPostAnswersResource($this);
 
-        return [
-            'id' => $this->id,
-            'is_request_valid' => $this->is_request_valid,
-            'cliamers' => new UserPostAnswersResource($this),
-            'date' => $this->created_at ? $this->created_at->toDateTimeString() : null,
-        ];
+       if($this->postRequestUser) 
+       return new UserPostAnswersResource($this);
+       else
+       return null;
+
+        // return [
+        //     'id' => $this->id,
+        //     'is_request_valid' => $this->is_request_valid,
+        //     'cliamers' => new UserPostAnswersResource($this),
+        //     'date' => $this->created_at ? $this->created_at->toDateTimeString() : null,
+        // ];
     }
 }
