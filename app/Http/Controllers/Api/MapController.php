be3d131ded02c7f66db799e28c1194443b42f3e2
Tarek Solaiman <t.mohamed@smartappco.com>
2020-09-10 12:46:42 +0000

    Fix map regex

--- app/Http/Controllers/Api/MapController.php
+++ app/Http/Controllers/Api/MapController.php
@@ -51,7 +51,7 @@ class MapController extends Controller
     public function __invoke(Request $request, $type = null)
     {
         $validate_request = Validator::make($request->all(), [
-            'longitude' => ['required', 'regex:/^[-]?(([0-8]?[0-9])\.(\d+))|(90(\.0+)?)$/'],
+            'longitude' => ['required', 'regex:/^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,9})?))$/'],
             'latitude' => ['required', 'regex:/^[-]?(([0-8]?[0-9])\.(\d+))|(90(\.0+)?)$/'],
             'radius' => ['required', 'integer'],
             'unit' => ['required', 'in:kilo,mile']
