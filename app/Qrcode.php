92e2e681dacd34b81902d6aa6414b5abe8f18105
Tarek Solaiman <t.mohamed@smartappco.com>
2020-09-15 12:06:13 +0000

    Fix QRcode Url

--- app/Qrcode.php
+++ app/Qrcode.php
@@ -70,7 +70,7 @@ class Qrcode extends Model implements QrcodeConstants
         return $this->belongsTo(PackageProductManagement::class, 'package_product_pivot_id');
     }
 
-    public function getUrlAttribute($value)
+    public function getQrcodeUrlAttribute($value)
     {
         return route('api.scan-qrcode-api', $value);
     }
