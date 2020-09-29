1e85544bc0d161ec6a624c41c5a2fc599468e4e5
Tarek Solaiman <t.mohamed@smartappco.com>
2020-09-29 12:13:13 +0000

    Update composer json

--- app/Nova/Notification.php
+++ app/Nova/Notification.php
@@ -70,8 +70,8 @@ class Notification extends Resource
             ID::make()->sortable(),
             Textarea::make('Body', 'body')->creationRules([
                 'required', 'min:2'
-            ]) ->showOnIndex()
-            ->readMore(),
+            ]) ->showOnIndex(),
+//            ->readMore(),
 
 
             RadioButton::make('Send To', 'send_to')
