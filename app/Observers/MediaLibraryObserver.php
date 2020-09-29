9efc9d3bd127940531833d15fb94aa005abcaecc
Tarek Solaiman <t.mohamed@smartappco.com>
2020-09-29 09:37:42 +0000

    Separate Corporate Media Library Images

---
+++ app/Observers/MediaLibraryObserver.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Observers;
+use ClassicO\NovaMediaLibrary\Core\Model;
+
+class MediaLibraryObserver
+{
+
+    public function saving(Model $model)
+    {
+        if (auth()->user()->isCorporateAdmin()){
+            $model->corporate_id = auth()->user()->corporate->id;
+        }
+    }
+}
