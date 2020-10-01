a6de874962791d6a5b4a11436edc1279da21e343
Tarek Solaiman <t.mohamed@smartappco.com>
2020-10-01 11:33:10 +0000

    Add My Profile Link

--- resources/views/vendor/nova/partials/user.blade.php
+++ resources/views/vendor/nova/partials/user.blade.php
@@ -13,6 +13,20 @@
 
 <dropdown-menu slot="menu" width="200" direction="rtl">
     <ul class="list-reset">
+        @if(auth()->user()->isAdmin())
+            <li>
+                <a href="wajad/resources/super-admins/{{auth()->user()->id }}" class="block no-underline text-90 hover:bg-30 p-3">
+                    My Profile
+                </a>
+            </li>
+        @endif
+        @if(auth()->user()->isCorporateAdmin())
+                <li>
+                    <a href="wajad/resources/users/{{auth()->user()->id }}" class="block no-underline text-90 hover:bg-30 p-3">
+                        My Profile
+                    </a>
+                </li>
+        @endif
         <li>
             <a href="{{ route('nova.logout') }}" class="block no-underline text-90 hover:bg-30 p-3">
                 {{ __('Logout') }}
