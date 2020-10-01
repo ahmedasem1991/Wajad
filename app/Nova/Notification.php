2006a8a6fd7793b2ce379e5bf8d17a4133e44682
IbrahemSaber <i.saber@smartappco.com>
2020-10-01 11:45:46 +0200

    solve merge confilict

--- app/Nova/Notification.php
+++ app/Nova/Notification.php
@@ -70,12 +70,8 @@ class Notification extends Resource
             ID::make()->sortable(),
             Textarea::make('Body', 'body')->creationRules([
                 'required', 'min:2'
-<<<<<<< HEAD
-            ]) ,
-=======
             ]) ->showOnIndex(),
 //            ->readMore(),
->>>>>>> 4037349928c7cbe984a64b871f75a4ffe0d5cb78
 
 
             RadioButton::make('Send To', 'send_to')
