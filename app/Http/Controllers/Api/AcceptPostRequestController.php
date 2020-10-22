05fc84372089d66fc24e6915ea64c10b105fc6c6
IbrahemSaber <i.saber@smartappco.com>
2020-10-22 14:10:24 +0200

    test with asif

--- app/Http/Controllers/Api/AcceptPostRequestController.php
+++ app/Http/Controllers/Api/AcceptPostRequestController.php
@@ -33,7 +33,8 @@ class AcceptPostRequestController extends Controller
     {
         //return $request->user_id;
         $validate_request = Validator::make($request->all(), [
-            'user_id' => ['required','exists:users,id'],
+           // 'user_id' => ['required','exists:users,id'],
+            'user_id' => ['required']
         ]);
 
         if ($validate_request->fails()) {
