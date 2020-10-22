741a78cd2bb9d168245c9b60734d8343923e362c
IbrahemSaber <i.saber@smartappco.com>
2020-10-22 17:11:28 +0200

    test qrcode

--- routes/web.php
+++ routes/web.php
@@ -701,9 +701,9 @@ return $response;
 //   ->eyeColor(0, 0,0, 0, 6,120, 160) 
 //   ->eyeColor( 1,0,0, 0, 6,120, 160)  
 //   ->eyeColor( 2,0,0, 0, 6,120, 160) 
-    ->style('dot',0.9)
+    //->style('dot',0.9)
     ->format('png')
-    ->merge(public_path('/images/wajadfinalogo.png'), 0.2, true)
+    ->merge(public_path('/images/wajadfinallogo.png'), 0.2, true)
     ->size(2000)
     ->generate(env('API_URL').'/api/scan-qr-code/'.$ImageName,
     public_path('images/qrcodes2/'.$ImageName))
