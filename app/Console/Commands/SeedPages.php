f6e5c11a3858b6974b35cadb713c8af0f4e9f642
tarek <tareksolaiman89@gmail.com>
2020-10-25 12:39:47 +0200

    Add Terms & Conditions Seeder

--- app/Console/Commands/SeedPages.php
+++ app/Console/Commands/SeedPages.php
@@ -71,6 +71,15 @@ class SeedPages extends Command
                 'created_at' => Carbon::now()->toDateTimeString(),
                 'updated_at' => Carbon::now()->toDateTimeString(),
             ],
+            [
+                'key' => 'terms',
+                'title_en' => 'Terms & Conditions',
+                'title_ar' => 'الشروط و اﻷحكام',
+                'body_en' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio autem ducimus nemo rem. Placeat aliquam delectus itaque illo nobis facere explicabo corporis qui praesentium nemo voluptates quod harum vel, culpa nesciunt sint tempora maxime non. Quidem nihil iure tenetur minus quia sint totam dolores magnam illo! Unde repellendus vitae consequuntur.',
+                'body_ar' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio autem ducimus nemo rem. Placeat aliquam delectus itaque illo nobis facere explicabo corporis qui praesentium nemo voluptates quod harum vel, culpa nesciunt sint tempora maxime non. Quidem nihil iure tenetur minus quia sint totam dolores magnam illo! Unde repellendus vitae consequuntur.',
+                'created_at' => Carbon::now()->toDateTimeString(),
+                'updated_at' => Carbon::now()->toDateTimeString(),
+            ],
 
         ];
 
