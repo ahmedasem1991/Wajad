cbc4499dbb48d57c0be6a68691932d06bbaab360
IbrahemSaber <i.saber@smartappco.com>
2020-10-26 11:24:47 +0200

    test

--- app/Http/Resources/MapResource.php
+++ app/Http/Resources/MapResource.php
@@ -21,7 +21,7 @@ class MapResource extends JsonResource
             'latitude' => $this->latitude,
             'longitude' => $this->longitude,
             // 'image' => (string) env("APP_URL") . "/" . $this->images ?? (string) $this->images()->first('image')['image'] ?? '',
-            'image' =>  env("APP_URL") . "/" .$this->images ? $this->images[0] :'',
+            'image' =>  env("APP_URL") . "/" .$this->images[0] ? $this->images[0] :'',
           
             'address' => $this->address ?? ''
         ];
