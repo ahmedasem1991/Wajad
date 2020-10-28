53aa15ff3d52afe5de66751ff27ea2bbf6133669
IbrahemSaber <i.saber@smartappco.com>
2020-10-28 15:43:15 +0200

    test

--- resources/views/webview/index.blade.php
+++ resources/views/webview/index.blade.php
@@ -22,8 +22,8 @@
 
 <p id="demo"></p>
 <script type="application/javascript">
-$.getJSON('https://ip-api.com/json?callback=?', function(data) {
-  console.log(data);
+$.getJSON('https://api.ipify.org?format=json', function(data){
+    console.log(data.ip);
 });
 
 
