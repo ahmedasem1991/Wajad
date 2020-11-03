874f3b7c20d458ba91657e300cb655e3d0c9038f
IbrahemSaber <i.saber@smartappco.com>
2020-11-03 10:38:48 +0200

    test

--- app/Http/Controllers/Api/AnswerController.php
+++ app/Http/Controllers/Api/AnswerController.php
@@ -75,7 +75,7 @@ class AnswerController extends Controller
         }, $request->data);
 
         
-        if($post->corporate_id !=NULL || $post->publisher()->isAdmin())
+        if($post->corporate_id !=NULL || $post->publisher->isAdmin())
         {
             //send Broadcast Notification
             $level='info';
