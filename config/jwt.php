5cb38b80f4d4d844cd8d9cfac32619a1d9a19850
IbrahemSaber <i.saber@smartappco.com>
2020-11-03 12:47:28 +0200

    JWT_BLACKLIST_ENABLED true

--- config/jwt.php
+++ config/jwt.php
@@ -222,7 +222,7 @@ return [
     |
     */
 
-    'blacklist_enabled' => env('JWT_BLACKLIST_ENABLED', false),
+    'blacklist_enabled' => env('JWT_BLACKLIST_ENABLED', true),
 
     /*
     | -------------------------------------------------------------------------
