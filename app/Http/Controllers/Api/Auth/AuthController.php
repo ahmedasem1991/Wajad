b2d1cbed086561a7d25e8885722607fc4362445f
IbrahemSaber <i.saber@smartappco.com>
2020-11-04 17:13:11 +0200

    test

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -195,7 +195,7 @@ class AuthController extends Controller
         ->where('deleted_at' ,NULL)->withTrashed()->first();
 
       
-        if( $User && ! $NormalUser)
+        if( $DeletedUser && ! $NormalUser)
         {
             $validate_request = Validator::make($request->all(), [
                 'name' => ['required', 'min:6', 'max:255'],
