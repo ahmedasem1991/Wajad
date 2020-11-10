51a7a222315fc88e8bcbc6abe502ae4e3d766fd6
IbrahemSaber <i.saber@smartappco.com>
2020-11-10 12:30:49 +0200

    test demo

--- resources/views/webview/index.blade.php
+++ resources/views/webview/index.blade.php
@@ -14,7 +14,7 @@
 
 </head>
 
-<body>
+<body >
 
 
     <script type="application/javascript">
@@ -178,6 +178,7 @@
                         </div>
                     </div>
                 </div>
+                <br>.
                 <div class="col-lg-6">
                     <div class="">
                         <div class="card border-0 mb-4">
