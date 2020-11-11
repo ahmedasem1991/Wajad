30903af0e22d3ea3f357e8799e7ecaab97e59de2
IbrahemSaber <i.saber@smartappco.com>
2020-11-11 16:20:57 +0200

    add with blue eyes

--- database/migrations/2019_09_25_144534_create_generate_qrcodes_table.php
+++ database/migrations/2019_09_25_144534_create_generate_qrcodes_table.php
@@ -19,6 +19,7 @@ class CreateGenerateQrcodesTable extends Migration
             $table->integer('type')->default(1);//default  (single)
             $table->string('status')->default('waiting');//finished
             $table->integer('quantity')->default(1);
+            $table->integer('blue_eyes')->default(0);
             $table->integer('created_by')->unsigned()->nullable();
             $table->string('created_from')->default('web');
            $table->softDeletes();
