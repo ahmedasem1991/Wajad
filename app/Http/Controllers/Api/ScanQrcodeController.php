fee456a667c3353b3030fbba10164705eea85972
IbrahemSaber <i.saber@smartappco.com>
2020-11-12 15:41:34 +0200

    test

--- app/Http/Controllers/Api/ScanQrcodeController.php
+++ app/Http/Controllers/Api/ScanQrcodeController.php
@@ -62,7 +62,7 @@ class ScanQrcodeController extends Controller
      */
     public function __invoke(Request $request, Qrcode $qr_code)
     {
-        return Carbon::now()->toDateTimeString();
+        return $qr_code->end_at;
         if ($qr_code->end_at < Carbon::now()->toDateTimeString()  ) {
             throw new ApiException(trans('messages.expired', ['model' => trans('messages.attributes.qrcode')]), 400);
         }
