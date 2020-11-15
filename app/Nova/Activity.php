61e7672590c3c69f8b6ab2daba69caf99a7dc99a
IbrahemSaber <i.saber@smartappco.com>
2020-11-15 16:16:31 +0200

    test

--- app/Nova/Activity.php
+++ app/Nova/Activity.php
@@ -55,7 +55,7 @@ class Activity extends Resource
     public static $searchRelations = [
         'user' => ['name', 'email', 'mobile_number'],
     ];
-    public static $perPageOptions = [100, 200, 300];
+    public static $perPageOptions = [20, 200, 300];
 
     /**
      * Get the fields displayed by the resource.
