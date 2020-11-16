0dbe89ff264391fe8fcaed77c8fd89bd0f5aa127
IbrahemSaber <i.saber@smartappco.com>
2020-11-16 15:23:16 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -89,7 +89,7 @@ Route::get('/smart-search/{search}', function ($search) {
     $array=[];
     $users=  User::normalusers()
     ->where('email' ,'LIKE', '%'.$search.'%')
-    ->orWhere('mobile_number','LIKE', '%'.$search.'%')->get()  ; 
+    ->orWhere('mobile_number','LIKE', '%'.$search.'%')->get(['name','id'])  ; 
 
     return  json_encode($users->toArray());
     if( $user)
