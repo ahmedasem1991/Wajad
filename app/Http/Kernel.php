a27230d26c10a3782bafb808f7647376bc6fff85
IbrahemSaber <i.saber@smartappco.com>
2020-12-01 10:19:18 +0200

    test edit phone

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,11 +38,12 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \Illuminate\Session\Middleware\StartSession::class,
             'throttle:60,1',
             'bindings',
             \App\Http\Middleware\AddHeaders::class,
             \App\Http\Middleware\SetLanguage::class,
-            \Illuminate\Session\Middleware\StartSession::class,
+            
         ],
     ];
 
