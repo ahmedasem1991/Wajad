84b4ffc1df5c43c09d9d299d522871f8d0854d53
IbrahemSaber <i.saber@smartappco.com>
2021-01-11 15:35:41 +0200

    test

--- app/Http/Resources/QuestionResource.php
+++ app/Http/Resources/QuestionResource.php
@@ -17,7 +17,7 @@ class QuestionResource extends JsonResource
         return [
             'id' => $this->id,
             'question' => $this->question,
-            'answer' =>  $this->answers->where('user_id',session()->get('request_u_id'))->toArray(),
+            'answer' =>  $this->answers->where('user_id',session()->get('request_u_id'))->toArray()[0]['answers'],
         ];
     }
 }
