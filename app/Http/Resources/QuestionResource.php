fbd07066b53c1332f2c525eb742cb6d157b92d46
IbrahemSaber <i.saber@smartappco.com>
2021-01-11 14:25:10 +0200

    test

--- app/Http/Resources/QuestionResource.php
+++ app/Http/Resources/QuestionResource.php
@@ -17,7 +17,7 @@ class QuestionResource extends JsonResource
         return [
             'id' => $this->id,
             'question' => $this->question,
-            'answer' =>  $this->question->answers()->first() ? $this->question->answers()->first()->answers : null,
+            'answer' =>  $this->question->answers->first() ? $this->question->answers->first()->answers : null,
         ];
     }
 }
