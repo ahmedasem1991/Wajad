4b8b0b16b865273b22f7ca756a9b6169655fb21f
IbrahemSaber <i.saber@smartappco.com>
2021-01-13 11:49:33 +0200

    test

--- app/Http/Controllers/Api/MesiboFileUploadController.php
+++ app/Http/Controllers/Api/MesiboFileUploadController.php
@@ -14,7 +14,7 @@ class MesiboFileUploadController extends Controller
             'file' => ['required'],
         ]);
         if ($validator->fails()) {          
-            return response()->json(['error'=>$validator->errors()], 403);                        
+            return response()->json(['error'=>$validator->errors()], 401);                        
          }
 
         $fileName = time().'.'.$request->file->extension();
