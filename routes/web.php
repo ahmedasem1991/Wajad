fd7fda3a9fa657b0f10cff0d2daa7574c90f06fa
IbrahemSaber <i.saber@smartappco.com>
2021-01-24 12:52:34 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,8 @@ Route::get('mesibo_add', function () {
 
    ;
     foreach( User::normalusers()->get() as $user)
-    {
+    { 
+        
         $url = "https://api.mesibo.com/api.php?op=useradd&token=".env('MESIBO_APP_TOKEN')."&addr=".$user->name.'-'.$user->id."&appid=com.smartappco.wajad&name=".$user->name;
         $client = new \GuzzleHttp\Client([
             'headers' => ['Content-Type' => 'application/json']
