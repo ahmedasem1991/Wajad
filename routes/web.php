4280e88a083c21f28310fca47536540fb61ffd31
IbrahemSaber <i.saber@smartappco.com>
2021-01-24 12:09:20 +0200

    test mesibo add

--- routes/web.php
+++ routes/web.php
@@ -64,6 +64,25 @@ Route::get('/home', function () {
     return  redirect(Nova::path());
 });
 
+Route::get('mesibo_add', function () {
+
+   ;
+    foreach( User::normalusers()->get() as $user)
+    {
+        $url = "https://api.mesibo.com/api.php?op=useradd&token=".env('MESIBO_APP_TOKEN')."&addr=".$user->name.'-'.$user->id."&appid=com.smartappco.wajad&name=".$user->name;
+        $client = new \GuzzleHttp\Client([
+            'headers' => ['Content-Type' => 'application/json']
+        ]);
+        $response = $client->get($url);
+        $response = json_decode($response->getBody(), true);
+        $user->mesibo_uid= $response['user']['uid']??null;
+        $user->mesibo_token= $response['user']['token']??null;
+        $user->mesibo_address= $user->name.'-'.$user->id;
+        $user->save();
+    }
+   
+});
+
 Route::view('qrcode', 'Pdf.qrcode');
 Auth::routes();
 //Test Notification
