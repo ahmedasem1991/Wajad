74d7cbaea2d83de4663d4ee091bbe72d9046e769
IbrahemSaber <i.saber@smartappco.com>
2021-01-27 10:39:04 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -386,7 +386,7 @@ Route::get('/asif_test', function (Request $request) {
         ->setSound('default');
 
     $dataBuilder = new PayloadDataBuilder();
-    $dataBuilder->addData(['data' => $data]);
+    $dataBuilder->addData($data);
 
     $option = $optionBuilder->build();
     $notification = $notificationBuilder->build();
