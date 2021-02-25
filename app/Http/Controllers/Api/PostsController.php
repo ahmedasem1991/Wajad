d8033dae1cef92c5582d537c7cd9d81e0ec5a82c
tarek <tareksolaiman89@gmail.com>
2021-02-25 11:39:52 +0200

    Update Validation

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -97,9 +97,9 @@ class PostsController extends Controller
         if ($type == "found") {
             $validate_found_post = Validator::make($request->all(), [
                 'questions' => ['required',  'array', 'between:1,3'],
-                'questions.0' => ['required', 'min:9', 'max:500'],
-                'questions.1' => ['min:9', 'max:500'],
-                'questions.2' => ['min:9', 'max:500'],
+                'questions.0' => ['required', 'min:4', 'max:500'],
+                'questions.1' => ['min:4', 'max:500'],
+                'questions.2' => ['min:4', 'max:500'],
             ]);
 
             if ($validate_found_post->fails()) {
@@ -114,9 +114,9 @@ class PostsController extends Controller
         if ($type == "found") {
             $validate_questions = Validator::make($request->all(), [
                 'questions' => ['required',  'array', 'between:1,3'],
-                'questions.0' => ['required', 'min:9', 'max:500'],
-                'questions.1' => ['nullable', 'min:9', 'max:500'],
-                'questions.2' => ['nullable', 'min:9', 'max:500'],
+                'questions.0' => ['required', 'min:4', 'max:500'],
+                'questions.1' => ['nullable', 'min:4', 'max:500'],
+                'questions.2' => ['nullable', 'min:4', 'max:500'],
             ]);
             if ($validate_questions->fails()) {
                 throw new ApiException($validate_questions->errors()->first(), 400);
@@ -540,9 +540,9 @@ class PostsController extends Controller
                 $type='found';
                 $validate_found_post = Validator::make($request->all(), [
                     'questions' => ['required',  'array', 'between:1,3'],
-                    'questions.0' => ['required', 'min:9', 'max:500'],
-                    'questions.1' => ['min:9', 'max:500'],
-                    'questions.2' => ['min:9', 'max:500'],
+                    'questions.0' => ['required', 'min:4', 'max:500'],
+                    'questions.1' => ['min:4', 'max:500'],
+                    'questions.2' => ['min:4', 'max:500'],
                 ]);
 
                 if ($validate_found_post->fails()) {
