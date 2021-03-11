a856e18bf49e7e82fab45a61d8f54c0f30f2b4f1
tarek <tareksolaiman89@gmail.com>
2021-03-11 13:23:49 +0200

    Test Mesibo Notification

--- app/Http/Controllers/Api/MesiboNotificationController.php
+++ app/Http/Controllers/Api/MesiboNotificationController.php
@@ -26,7 +26,7 @@ class MesiboNotificationController extends Controller
     public function  __invoke(Request $request)
     {
         try {
-            DB::table('integration_temp')->insert($request->data);
+            DB::table('mesibo_notification')->insert(['payload' => $request->all()]);
             return 'MESIBO OK';
         } catch (\Exception $e) {
             return $e->getMessage();
