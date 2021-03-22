68bdbfebd4e43872e99c39449a61afbcf609d4fc
IT Services <it@sltc.sa>
2021-03-22 17:16:33 +0200

    test

--- app/Console/Commands/FillBrandwatch.php
+++ app/Console/Commands/FillBrandwatch.php
@@ -41,54 +41,32 @@ class FillBrandwatch extends Command
     {
         $data = array(
             0 => array(
-                'name_en' => 'Bertram',
-                'name_ar' => 'Bertram',
-                'image' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
+                'Trek' => 'Connondale',
+                '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
             ),
             1 => array(
-                'name_en' => 'Boston Whaler',
-                'name_ar' => 'Boston Whaler',
-                'image' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
+                'Trek' => 'Kona Bikes',
+                '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
             ),
             2 => array(
-                'name_en' => 'Chaparral',
-                'name_ar' => 'Chaparral',
-                'image' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
+                'Trek' => 'Colnago',
+                '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
             ),
             3 => array(
-                'name_en' => 'Grady-White',
-                'name_ar' => 'Grady-White',
-                'image' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
+                'Trek' => 'Bianchi',
+                '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
             ),
             4 => array(
-                'name_en' => 'Lund',
-                'name_ar' => 'Lund',
-                'image' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
+                'Trek' => 'Raleigh',
+                '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
             ),
             5 => array(
-                'name_en' => 'Mastercraft',
-                'name_ar' => 'Mastercraft',
-                'image' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
+                'Trek' => 'Cervelo',
+                '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
             ),
             6 => array(
-                'name_en' => 'Sea Ray',
-                'name_ar' => 'Sea Ray',
-                'image' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
-            ),
-            7 => array(
-                'name_en' => 'Tracker',
-                'name_ar' => 'Tracker',
-                'image' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
-            ),
-            8 => array(
-                'name_en' => 'Yamaha',
-                'name_ar' => 'Yamaha',
-                'image' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
-            ),
-            9 => array(
-                'name_en' => 'Viking Yachts',
-                'name_ar' => 'Viking Yachts',
-                'image' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
+                'Trek' => 'Orbea',
+                '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png' => '/images/action-028-detail-more-info-others-512-1606640534-QDKIL.png'
             ),
         );
       //  $subcategory = SubCategory::find(4);
@@ -96,7 +74,7 @@ class FillBrandwatch extends Command
             
             // $subcategory->brands()->createMany($data);
         //}
-        $subcategory = SubCategory::find(34);
+        $subcategory = SubCategory::find(31);
         $subcategory->brands()->createMany($data);
          
 
