e90440002add3b4d8f24a02ee81246d7eb91626e
tarek <tareksolaiman89@gmail.com>
2021-03-25 13:53:24 +0200

    Test

--- app/Services/Helpers/Traits/ModelObserveImage.php
+++ app/Services/Helpers/Traits/ModelObserveImage.php
@@ -7,14 +7,14 @@ trait ModelObserveImage
 {
     public static function bootModelObserveImage()
     {
-        self::saved(function ($model){
-            $img = Image::make(public_path($model->image));
-            if($img->width() > 100){
-                $img->resize(100, null, function ($con){
-                    $con->aspectRatio();
-                });
-                $img->save();
-            }
-        });
+//        self::saved(function ($model){
+//            $img = Image::make(public_path($model->image));
+//            if($img->width() > 100){
+//                $img->resize(100, null, function ($con){
+//                    $con->aspectRatio();
+//                });
+//                $img->save();
+//            }
+//        });
     }
 }
