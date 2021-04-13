bc09e8e65a08b76802658172ceb5dba27ca04011
IT Services <it@sltc.sa>
2021-04-13 14:43:37 +0200

    test

--- app/Nova/Stock.php
+++ app/Nova/Stock.php
@@ -29,7 +29,7 @@ class Stock extends Resource
      * @var string
      */
     public static $model = 'App\Qrcode';
-    public static $perPageOptions = [50, 100, 150];
+    public static $perPageOptions = [100, 200, 300];
     /**
      * The logical group associated with the resource.
      *
