1d61030207e906c3726f683357eb1c360c116e92
IT Services <it@sltc.sa>
2021-04-14 13:18:48 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -156,7 +156,7 @@ Route::get('/smart-search/{search}', function ($search) {
 Route::get('/test600', function (Request $request) {
 
 
-    $brands=Brand::all();
+    $brands=Brand::take(500)->skip(600)->get();
     foreach($brands as $brand)
     {
 
