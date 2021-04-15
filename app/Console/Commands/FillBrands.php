a153bafe372e2ac0ddfbabb7997bcb04bb610881
IT Services <it@sltc.sa>
2021-04-15 13:52:45 +0200

    test

--- app/Console/Commands/FillBrands.php
+++ app/Console/Commands/FillBrands.php
@@ -41,33 +41,23 @@ class FillBrands extends Command
     public function handle()
     {
         $data =array(
-            0 => array('name_en' => 'Brighton', 'name_ar' => 'برايتون'),
-            1 => array('name_en' => 'Burberry', 'name_ar' => 'بربري'),
-            2 => array('name_en' => 'Calvin Klein', 'name_ar' => 'كالفن كلاين'),
-            3 => array('name_en' => 'Chanel', 'name_ar' => 'شانيل'),
-            4 => array('name_en' => 'Chloé', 'name_ar' => 'كلوي'),
-            5 => array('name_en' => 'Coach', 'name_ar' => 'كوتش'),
-            6 => array('name_en' => 'Coach Factory', 'name_ar' => 'كوتش فاكتوري'),
-            7 => array('name_en' => 'Cole Haan', 'name_ar' => 'كول هان'),
-            8 => array('name_en' => 'Dooney & Bourke', 'name_ar' => 'دوني اند بورك'),
-            9 => array('name_en' => 'Fendi', 'name_ar' => 'فيندي'),
-            10 => array('name_en' => 'Fossil', 'name_ar' => 'فوسيل'),
-            11 => array('name_en' => 'Furla', 'name_ar' => 'فورلا'),
-            12 => array('name_en' => 'Gucci', 'name_ar' => 'غوتشي'),
-            13 => array('name_en' => 'Kate Spade New York', 'name_ar' => 'كيت سبيد نيويورك'),
-            14 => array('name_en' => 'Longchamp', 'name_ar' => 'لونج شامب'),
-            15 => array('name_en' => 'Louis Vuitton', 'name_ar' => 'لويس فويتون'),
-            16 => array('name_en' => 'Marc by Marc Jacobs', 'name_ar' => 'مارك باي مارك جاكوبس'),
-            17 => array('name_en' => 'Michael Kors', 'name_ar' => 'مايكل كورس'),
-            18 => array('name_en' => 'Nine West', 'name_ar' => 'ناين وست'),
-            19 => array('name_en' => 'Prada', 'name_ar' => 'برادا'),
-            20 => array('name_en' => 'Rebecca Minkoff', 'name_ar' => 'ريبيكا مينكوف'),
-            21 => array('name_en' => 'Salvatore Ferragamo', 'name_ar' => 'سلفاتوري فيراغامو'),
-            22 => array('name_en' => 'Ted Baker', 'name_ar' => 'تيد بيكر'),
-            23 => array('name_en' => 'Tory Burch', 'name_ar' => 'توري بورش'),
-            24 => array('name_en' => 'Other', 'name_ar' => 'أخرى'),
+            0 => array('name_en' => 'Victorinox', 'name_ar' => 'فسكتورنوكس'),
+            1 => array('name_en' => 'American Tourister', 'name_ar' => 'أمريكان توريستر'),
+            2 => array('name_en' => 'Samsonite', 'name_ar' => 'سامسونايت'),
+            3 => array('name_en' => 'Travelpro', 'name_ar' => 'ترافلبرو'),
+            4 => array('name_en' => 'Eagle Creek', 'name_ar' => 'إيجل كريك'),
+            5 => array('name_en' => 'Delsey', 'name_ar' => 'ديلسي'),
+            6 => array('name_en' => 'Briggs & Riley', 'name_ar' => 'بريجز اند رايلي'),
+            7 => array('name_en' => 'Victorinox', 'name_ar' => 'فسكتورنوكس'),
+            8 => array('name_en' => 'Tumi', 'name_ar' => 'تومي'),
+            9 => array('name_en' => 'Hartmann', 'name_ar' => 'هارتمان'),
+            10 => array('name_en' => 'Bric’s', 'name_ar' => 'بريتش'),
+            11 => array('name_en' => 'Rimowa', 'name_ar' => 'ريموا'),
+            12 => array('name_en' => 'Globe-Trotter', 'name_ar' => 'غلوب تروتر'),
+            13 => array('name_en' => 'Away', 'name_ar' => 'أواي'),
+            14 => array('name_en' => 'Others', 'name_ar' => 'أخرى'),
         );
-        $subcategory = SubCategory::find(8);
+        $subcategory = SubCategory::find(9);
          
         foreach ($data as $item){
             $brand = Brand::create($item);
