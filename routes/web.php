8fa8fd21d2972671d0d63250ee6a5d5c0520b5fe
IT Services <it@sltc.sa>
2021-08-02 13:12:02 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ Route::get('/home', function () {
 });
 
 Route::get('mesibo_add', function () {
-    $url = "https://api.mesibo.com/api.php?op=useradd&token=".env('MESIBO_APP_TOKEN')."&addr=".'$user->name'.'-'.'test 555'."&appid=com.smartappco.wajad&name=".'$user->name';
+    $url = "https://api.mesibo.com:443/api.php?op=useradd&token=".env('MESIBO_APP_TOKEN')."&addr=".'$user->name'.'-'.'test 555'."&appid=com.smartappco.wajad&name=".'$user->name';
     $client = new \GuzzleHttp\Client([
        'headers' => [
            //'Content-Type' => 'application/json',
