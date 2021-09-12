4ef7c9a0eac64449f89ab9f0fb464927be8556a1
IT Services <it@sltc.sa>
2021-09-12 14:59:36 +0200

    test

--- resources/views/contact_us.blade.php
+++ resources/views/contact_us.blade.php
@@ -2,28 +2,25 @@
 <html lang=”en”>
 <head>
 <meta charset=”UTF-8″>
-<meta name=”viewport” content=”width=device-width, initial-scale=1.0″>
-<meta http-equiv=”X-UA-Compatible” content=”ie=edge”>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>CONTACT US</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
 <div class=”container”>
 
-<div class="content">
-   <div class="row">
-     <div class="col-md-12">
-       <div class="card card-user">
+ 
+      
          <div class="card-header">
            <h5 class="card-title">Contact Us</h5>
          </div>
-        <div class="card-body">
+         <br>
 
         <div class="text-center">
                             <center> <img src="https://admin.wajad.co/images/wajad_logo.png" style="height:50px;margin-left:auto;margin-right: auto;display:block;float:snap" alt="Avatar"></center>
                     </div>
 
-
+<br>
                     
            @if(Session::has('success'))
               <div class="alert alert-success">
@@ -33,8 +30,8 @@
            
           <form method="post" action="contact-us">
              {{csrf_field()}}
-             <div class="row  ">
-             <div class="col-md-6 update ml-auto mr-auto">
+             <div class="row  " style="margin: 10px;">
+             <div class="col-md-6 update ml-auto mr-auto" >
                
                  <div class="form-group">
                    <label> Name </label>
@@ -98,11 +95,8 @@
              </div>
            </form>
          </div>
-       </div>
-     </div>
-   </div>
-</div>
+     
 
-</div>
+ 
 </body>
 </html>
