76565ac7199e3e576c9b03b8cc64bc53714f30c4
IT Services <it@sltc.sa>
2021-10-20 01:23:56 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -410,6 +410,10 @@ Route::get('/broadcast', function () {
 });
 Route::get('/asif_test', function (Request $request) {
 
+    if (!extension_loaded('imagick')){
+        echo 'imagick not installed';
+    }
+    dd('test');
      Qrcode::where('status','10')->chunk(1000, function($Qrcodes) {
         foreach ($Qrcodes as $Qrcode) {
             $Qrcode->status=10;
