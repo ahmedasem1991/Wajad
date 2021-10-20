354d7e8a2385700c201219410fdfd15bd349f5c7
IT Services <it@sltc.sa>
2021-10-20 10:50:41 +0200

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command(GenerateQRCodes::class)->daily();
         $schedule->command(TestSchedule::class)->daily();
-        $schedule->command('schedule:daily')->daily();
+        $schedule->command('schedule:daily')->everyMinute();
 
     }
 
