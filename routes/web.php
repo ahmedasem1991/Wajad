6d8bd481606f4c9c0628819b9f169cc0f7cfdc7a
IbrahemSaber <ibrahim.saber512@outlook.com>
2022-03-24 14:57:35 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -96,7 +96,7 @@ Route::get('mesibo_add', function () {
  
    
     $response = $client->get($url);
-    dd( $response['result']);
+    return( $response);
     ;
     foreach( User::normalusers()->get() as $user)
     {
