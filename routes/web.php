b5b300d08ae02b0a03eac9e0903187d380bc8557
IbrahemSaber <ibrahim.saber512@outlook.com>
2022-03-24 15:07:24 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -69,7 +69,7 @@ Route::get('/home', function () {
 });
 
 Route::get('mesibo_add', function () {
-    $url = "https://api.mesibo.com/api.php?op=useradd&token=kyiy639elg9i7g4r4wes6swhknerfgzhr1enoorf1zwc67eitl1wj5kkg3vnop2j&addr=ahmed-test555&appid=com.smartappco.wajad&name=ahmed";
+    $url = "https://api.mesibo.com/api.php?op=useradd&token=kyiy639elg9i7g4r4wes6swhknerfgzhr1enoorf1zwc67eitl1wj5kkg3vnop2j&addr=ahmed-test578555&appid=com.smartappco.wajad&name=ahmed";
     $client = new \GuzzleHttp\Client([
        'headers' => ['Content-Type' => 'application/json']
     ]);
