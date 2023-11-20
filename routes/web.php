42610a9b5f931237d565d2981e41e930d01e8159
IbrahemSaber <ibrahim.saber512@outlook.com>
2023-11-20 12:41:37 +0200

    fe

--- routes/web.php
+++ routes/web.php
@@ -184,6 +184,7 @@ Route::get('/smart-search/{search}', function ($search) {
 });
 
 Route::get('/test600', function (Request $request) {
+    Mail::to('ibrahim.saber512@outlook.com')->send(new MailAdminNotification('test'));
     dd(\Unifonic::send('966504334115', 'test message'))   ;
  $string='01142416124';
 $string2=   ltrim((string) $string, 0);
