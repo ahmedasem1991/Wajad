2ef4c8ba16a054434a38c24af6bacb5af2ff4909
IbrahemSaber <ibrahim.saber512@outlook.com>
2024-01-08 11:00:25 +0200

    fef

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -93,7 +93,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
 
                 if (Auth()->user()->hasPermissionTo('settings')) {
                    // array_push($array,new \Tightenco\NovaGoogleAnalytics\PageViewsMetric);
-                    array_push($array,new \Tightenco\NovaGoogleAnalytics\VisitorsMetric);
+                    // array_push($array,new \Tightenco\NovaGoogleAnalytics\VisitorsMetric);
                     //array_push($array,new \Tightenco\NovaGoogleAnalytics\MostVisitedPagesCard);
                     array_push($array,new ActivationDevices);
                     array_push($array, (new GoogleMaps)->markers($Corporates)->offices($Offices));
