dfdb8ff325ff7d3a181a68cbe745d35218185f85
IbrahemSaber <ibrahim.saber512@outlook.com>
2024-01-14 16:59:03 +0200

    fef

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -104,7 +104,7 @@ class UserService
 
 
             if($user->country->country_code==="966" || $user->country->country_code==="+966"){
-                Unifonic::send($user->country->country_code. $user->mobile_number, $message, 'WAJAD');
+                \Unifonic::send($user->country->country_code. $user->mobile_number, $message, 'WAJAD');
                 // new SendSMSEvent( $user->country->country_code. $user->mobile_number,$message);
             }
             else{
